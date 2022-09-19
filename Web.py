c1daf25a5a5653d1821c1864ab5e0d336fe2081c
Kiko <krucek2005@gmail.com>
2022-09-19 22:18:29 +0200

    Dnes sa moc nedari

--- Web.py
+++ Web.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, Markup
 import chess
 import chess.svg
 import sys
+from markupsafe import escape
+
 
 
 app = Flask(__name__)
@@ -10,13 +12,14 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {name}!"
 
 @app.route("/chess")
 def funkica_meno():
     return render_template('template.html', num=12)
+
+@app.route("/user")
+def user():
+    return "<h1>Welcome user of this website</h1> "
  
 if __name__ == '__main__':
     app.run( debug=True )
