9a3a15e644dc6339fb7e572ca2964ebbaa540826
Kiko <krucek2005@gmail.com>
2022-09-28 21:57:29 +0200

    uz sa zlepsujeme

--- Web.py
+++ Web.py
@@ -3,23 +3,54 @@ import chess
 import chess.svg
 import sys
 from markupsafe import escape
-
-
+from flask import url_for
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+    
+
+@app.route("/world")
+def world():
+    return "<h1>Hello, World<h1>" 
 
 
 @app.route("/chess")
 def funkica_meno():
     return render_template('template.html', num=12)
 
-@app.route("/user")
-def user():
-    return "<h1>Welcome user of this website</h1> "
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{escape(username)}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('profile', username='Kristian Rucek'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
  
 if __name__ == '__main__':
     app.run( debug=True )
