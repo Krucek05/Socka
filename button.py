954f6ee674fe14461a65e9e9889d69eee0d12347
Kiko <krucek2005@gmail.com>
2022-10-03 22:40:56 +0200

    robi sa na sachovnici

--- button.py
+++ button.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 def button():
     if request.method == 'POST':
-        if request.form.get('action1') == 'See text':
-            pass
+       # if request.form.get('button') == 'See text':
+             return render_template("textforbutton.html")
     elif request.method == 'GET':
-        return render_template('button.html', )
+        return render_template('button.html')
 
     return render_template ("button.html", form=form)
 
